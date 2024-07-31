887d52255b47c3bd83764e1dd8c872046b4557e5
Gerges-Creative <gerges.zechariah@gmail.com>
2024-07-31 00:30:43 +0000

    Basic list of objects

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -4,10 +4,10 @@
  */
 
 export default function getListStudents() {
-    studentsList = [
+    const studentsList = [
   { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
   { id: 2, firstName: 'James', location: 'Columbia' },
-  { id: 5, firstName: 'Serena', location: 'San Francisco' }
+  { id: 5, firstName: 'Serena', location: 'San Francisco' },
     ];
 
     return studentsList;
