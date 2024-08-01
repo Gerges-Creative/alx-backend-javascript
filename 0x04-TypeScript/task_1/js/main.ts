f4303e71ae89d3bfa05e19935f8dee66a2620e1e
Gerges-Creative <gerges.zechariah@gmail.com>
2024-08-01 00:24:20 +0000

    Task 2

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -6,3 +6,40 @@ export interface Teacher {
   location: string;
   [key: string]: any;
   }
+
+export interface Directors extends Teacher {
+  numberOfReports: number;
+  }
+
+export interface PrintTeacherFunction {
+  (firstName: string, lastName: string): string;
+  }
+
+export function printTeacher(firstName: string, lastName: string) : string {
+  return `${firstName[0]}. ${lastName}`;
+  }
+
+export interface StudentClassConstructor {
+  new (firstName: string, lastName: string): StudentClassInterface;
+  }
+
+export interface StudentClassInterface {
+  workOnHomework(): string;
+  displayName(): string;
+  }
+
+export class StudentClass implements StudentClassInterface {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.f = firstName;
+    this.l = lastName;
+  }
+  workOnHomework() {
+  return "Currently working";
+  }
+  displayName() {
+  return this.f;
+  }
+}
