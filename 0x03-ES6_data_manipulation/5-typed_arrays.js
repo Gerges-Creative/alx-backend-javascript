492a8fe10758ff0ff4550456c65d7070f04e88db
Gerges-Creative <gerges.zechariah@gmail.com>
2024-08-02 23:36:27 +0000

    Typed Arrays

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -7,13 +7,13 @@
  */
 
 export default function createInt8TypedArray(length, position, value) {
-  const buffer = new ArrayBuffer(length);
-  const int8view = new DataView(buffer);
-
   if (position >= length) {
     throw new Error('Postion outside range');
   }
 
+  const buffer = new ArrayBuffer(length);
+  const int8view = new DataView(buffer);
   int8view.setInt8(position, value);
+
   return int8view;
 }
