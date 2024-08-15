b96fe4d8bc06488553d5d4cc5ba3bde10b12eb12
Gerges-Creative <gerges.zechariah@gmail.com>
2024-08-15 22:52:43 +0000

    Map data structure

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -3,3 +3,13 @@
  * @returns {Map<string, Number>}
  */
 export default function groceriesList() {
+  const items = new Map([
+    ["Apples", 10],
+    ["Tomatoes", 10],
+    ["Pasta", 1],
+    ["Rice", 1],
+    ["Banana", 5]
+  ]);
+
+  return items;
+}
